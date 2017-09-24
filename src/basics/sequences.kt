0d59be8427a12ee5fcd4c2bf189594b0038e39c4
Alberto Muñoz <alberto.mqo@gmail.com>
2017-09-24 14:10:03 +0200

    Example of different use of sequences

---
+++ src/basics/sequences.kt
@@ -0,0 +1,36 @@
+package basics
+
+fun main(args: Array<String>) {
+
+    val veryLongList = (1..99999999L).toList()
+
+    val sum = veryLongList
+            .take(1000)
+            .filter { it > 50 }
+            .map { it * 2 }
+            .sum()
+
+    println(sum)
+
+
+    // This one is better than the previous to calculate the result
+    // but does not avoid the time spent allocating memory
+    val sumLazy = veryLongList
+            .asSequence()
+            .take(1000)
+            .filter { it > 50 }
+            .map { it * 2 }
+            .sum()
+
+    println(sumLazy)
+
+
+    // This one avoids the time spent by the previous two allocating memory for the veryLongList
+    val sumSeq = generateSequence(1, { it + 1 })
+            .take(1000)
+            .filter { it > 50 }
+            .map { it * 2 }
+            .sum()
+
+    println(sumSeq)
+}
