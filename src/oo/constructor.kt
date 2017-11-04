f52279b9e4847ed0875d043b02b085b794c69d80
ALTRAN_ES\alberto.quiros <alberto.munoz@altran.com>
2017-11-04 13:16:03 +0100

    Class implementation through constructor

--- src/oo/constructor.kt
+++ src/oo/constructor.kt
@@ -2,4 +2,15 @@ package oo
 
 class Country(val name: String, val population: Int) {
 
+    fun print() = "$name, $population habs"
+}
+
+fun main(args: Array<String>) {
+
+    val australia = Country("Australia", 24_561_509)
+
+    println(australia.name)
+    println(australia.population)
+
+    println(australia.print())
 }
