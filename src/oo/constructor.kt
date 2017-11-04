0d075d8e9c7d14e819b807a30f2331c18b24bc23
ALTRAN_ES\alberto.quiros <alberto.munoz@altran.com>
2017-11-04 13:10:55 +0100

    Class constructor the Java way

---
+++ src/oo/constructor.kt
@@ -0,0 +1,12 @@
+package oo
+
+class Country(name: String, population: Int) {
+
+    val name: String
+    val population: Int
+
+    init {
+        this.name = name
+        this.population = population
+    }
+}
