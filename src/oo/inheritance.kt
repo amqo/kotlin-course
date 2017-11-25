632b8b4cd9107d0028b116740c69e5ed8ff49e3a
ALTRAN_ES\alberto.quiros <alberto.munoz@altran.com>
2017-11-25 19:49:55 +0100

    Abstract classes and methods example

--- src/oo/inheritance.kt
+++ src/oo/inheritance.kt
@@ -6,20 +6,20 @@ package oo
 open class Base
 class Child: Base()
 
-open class Shape(val name: String) {
+open class Shape_(val name: String) {
 
     // "open" here allows this method to be overriden in child classes
     open fun area() = 0.0
 }
 
-class Circle(name: String, val radius: Double): Shape(name) {
+class Circle_(name: String, val radius: Double): Shape_(name) {
 
     override fun area() = Math.PI * Math.pow(radius, 2.0)
 }
 
 fun main(args: Array<String>) {
 
-    val smallCircle = Circle("Small Circle", 2.0)
+    val smallCircle = Circle_("Small Circle", 2.0)
 
     println(smallCircle.name)
     println(smallCircle.radius)
