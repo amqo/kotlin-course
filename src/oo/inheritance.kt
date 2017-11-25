96030af172a485f31105bc195f2c210e00b53e8b
ALTRAN_ES\alberto.quiros <alberto.munoz@altran.com>
2017-11-25 19:37:47 +0100

    Method inheritance

--- src/oo/inheritance.kt
+++ src/oo/inheritance.kt
@@ -4,6 +4,25 @@ package oo
 
 // By default classes in Kotlin are final, so we need to add this "open"
 open class Base
-
 class Child: Base()
+
+open class Shape(val name: String) {
+
+    // "open" here allows this method to be overriden in child classes
+    open fun area() = 0.0
+}
+
+class Circle(name: String, val radius: Double): Shape(name) {
+
+    override fun area() = Math.PI * Math.pow(radius, 2.0)
+}
+
+fun main(args: Array<String>) {
+
+    val smallCircle = Circle("Small Circle", 2.0)
+
+    println(smallCircle.name)
+    println(smallCircle.radius)
+    println(smallCircle.area())
+}
 
