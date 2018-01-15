070ff5c8cc629bf7a8537182867171dfaafa4381
ALTRAN_ES\alberto.quiros <alberto.munoz@altran.com>
2018-01-15 11:30:46 +0100

    Generic classes example

---
+++ src/oo/generics.kt
@@ -0,0 +1,29 @@
+package oo
+
+import java.sql.Time
+import java.util.*
+
+// Here List is a generic class, as you can use it for different List types
+var list: List<Int> = listOf(1, 2, 3)
+
+// <E> here is the generic type parameter
+// Usually <E> for element, <T> for type and so on
+class Timeline<E> {
+
+    val date2Data: MutableMap<Date, E> = mutableMapOf()
+
+    fun add(element: E) {
+        date2Data.put(Date(), element)
+    }
+
+    fun getLatest(): E {
+        return date2Data.values.last()
+    }
+}
+
+fun main(args: Array<String>) {
+
+    val timeline: Timeline<Int> = Timeline()
+    timeline.add(2)
+    println(timeline.getLatest())
+}
